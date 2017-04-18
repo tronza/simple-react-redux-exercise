cde0c7afb3cdad2570d6d9847aacdbc56579614b
Kai Kulju <kai.kulju@terveystalo.com>
2017-04-18 15:05:54 +0300

    Added initial hello world full stack demo

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: __dirname + '/src/index.js',
+    output: {
+        path: __dirname + '/public/',
+        filename: 'index.js',
+        publicPath: '/dist/'
+    },
+
+    devServer: {
+        inline: true,
+        port: 8666
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'stage-2']
+                }
+            }
+        ]
+    }
+};
